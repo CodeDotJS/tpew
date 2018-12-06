87e703bcee49bea90d821ff2ae0fd626554a098f
CodeDotJS <rushforlinux@gmail.com>
2018-12-07 00:46:57 +0530

    pass

---
+++ test.js
@@ -0,0 +1,35 @@
+import childProcess from 'child_process';
+import test from 'ava';
+
+test.cb('now', t => {
+	const cp = childProcess.spawn('./cli.js', ['--now'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('stat', t => {
+	const cp = childProcess.spawn('./cli.js', ['--stat'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
+
+test.cb('offline', t => {
+	const cp = childProcess.spawn('./cli.js', ['--stat', '--offline'], {stdio: 'inherit'});
+
+	cp.on('error', t.ifError);
+
+	cp.on('close', code => {
+		t.is(code, 0);
+		t.end();
+	});
+});
