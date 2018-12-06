27268d6599213515f7887bd84efb1f9e0c410bab
CodeDotJS <rushforlinux@gmail.com>
2018-12-07 01:19:38 +0530

    remove --stat

--- test.js
+++ test.js
@@ -12,17 +12,6 @@ test.cb('now', t => {
 	});
 });
 
-test.cb('stat', t => {
-	const cp = childProcess.spawn('./cli.js', ['--stat'], {stdio: 'inherit'});
-
-	cp.on('error', t.ifError);
-
-	cp.on('close', code => {
-		t.is(code, 1);
-		t.end();
-	});
-});
-
 test.cb('offline', t => {
 	const cp = childProcess.spawn('./cli.js', ['--stat', '--offline'], {stdio: 'inherit'});
 
